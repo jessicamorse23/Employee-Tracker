9ff0e4136ebfef8b456b1943979210395d1a5503
[Jessica Morse] <[jessicabrookemorse@gmail.com]>
2022-10-03 07:35:45 -0400

    added intital inquirer prompt to index.js

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
-const inquirer = require('inquirer');
-const mysql = require('mysql2');
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
+const cTable = require("console.table");
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    port: 3301,
+    user: "root",
+    password: "rootroot",
+    database: "employeeDb",
+  },
+  console.log("connected")
+);
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    database: 'test'
+
+// questions for user 
+const question = [
+  {
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["View departments", "View roles", "View employees", "Add department", "Add role", "Add employee"],
+    name: "choice",
+  },
+];
+
+function init() {
+  inquirer.prompt(question).then((response) => {
+    switch (response.choice) {
+      case "View departments":
+        break;
+      case "View roles":
+        break;
+      case "View employees":
+        break;
+      case "Add department":
+        break;
+      case "Add employee":
+        break;
+      case "Add role":
+        break;
+    }
   });
+}
+
+
 // database file (DB)
 // front end folder
 //  back end folder
-
 
 // create the commands required to view the table
 // create the database with the tables with the expected properties that each table needs
@@ -17,15 +53,10 @@ const connection = mysql.createConnection({
 // work on the prompts for adding to the table
 // file with helper functions
 
-connection.execute(
-    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
-    [],
-    function(err, results, fields) {
-      console.log(results); // results contains rows returned by server
-      console.log(fields); // fields contains extra meta data about results, if available
-  
-      // If you execute same statement again, it will be picked from a LRU cache
-      // which will save query preparation time and give better performance
-    }
-  );
+connection.execute("SELECT * FROM `table` WHERE `name` = ? AND `age` > ?", [], function (err, results, fields) {
+  console.log(results); // results contains rows returned by server
+  console.log(fields); // fields contains extra meta data about results, if available
 
+  // If you execute same statement again, it will be picked from a LRU cache
+  // which will save query preparation time and give better performance
+});
